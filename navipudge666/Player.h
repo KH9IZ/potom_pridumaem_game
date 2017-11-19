8537c030859486d43f032f293de5e18778aeb9c6
navipudge666 <33724392+navipudge666@users.noreply.github.com>
2017-11-19 12:36:49 +0500

    поменял контрол!!!
    Замедление на LShift

--- navipudge666/Player.h
+++ navipudge666/Player.h
@@ -11,23 +11,26 @@ using namespace sf;
 class Player: public Entity
 {
 	public:
-		Player(std::string file,double X,double Y): Entity(file, X, Y) { }
+		Player(std::string file,double X,double Y): Entity(file, X, Y) {}
+		
 		
 		void cok()
 		{
-			std::cout<<x<<' '<<y<<std::endl;
+			std::cout<<x<<' '<<y<<' '<<speed<<std::endl;
 		}
 		
 		void control(float time)
 		{
-			if (Keyboard::isKeyPressed(Keyboard::Up) && (y > h))
-				y -= 0.05*time; 
-			if (Keyboard::isKeyPressed(Keyboard::Down) && (y < 600-h))
-				y += 0.05*time;
-			if (Keyboard::isKeyPressed(Keyboard::Left) && (x > w))
-				x -= 0.05*time;
-			if (Keyboard::isKeyPressed(Keyboard::Right) && (x <= 800-w))
-				x += 0.05*time;
+			if (Keyboard::isKeyPressed(Keyboard::Up) && (y-speed*time > h))
+				y -= speed*time; 
+			if (Keyboard::isKeyPressed(Keyboard::Down) && (y+speed*time < 600-h))
+				y += speed*time;
+			if (Keyboard::isKeyPressed(Keyboard::Left) && (x-speed*time > w))
+				x -= speed*time;
+			if (Keyboard::isKeyPressed(Keyboard::Right) && (x+speed*time <= 800-w))
+				x += speed*time;
+			if (Keyboard::isKeyPressed(Keyboard::LShift))
+				speed=2.5/100; else speed=3.75/100;
 			sprite.setPosition(x,y);
 		}
 };
