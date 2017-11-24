ce60e837e71bf8718faadfda3819c8fbf96387f9
navipudge666 <vovan.grekalov@gmail.com>
2017-11-24 18:52:18 +0500

    живу в России B-)

--- navipudge666/main.cpp
+++ navipudge666/main.cpp
@@ -1,5 +1,6 @@
 #include "Entity.h"
 #include "Player.h"
+#include "SimpleBullet.h"
 #include <SFML/Graphics.hpp>
 
 using namespace sf;
@@ -9,16 +10,20 @@ int main()
 	RenderWindow window(VideoMode(800,600), "Potom Pridumaem");
 	Event event;
 	Clock clock;
-	
+    double reload_time;
+
 	Player cmonBruh("cmonBruh.png",0,0,14,14);
 	
 	Player entity("chai_pyu.png",400,400,15,15);
-	
+
+    std::vector<SimpleBullet> bullets{};
+
 	while (window.isOpen())
 	{
-		float time=clock.getElapsedTime().asMicroseconds();
-		clock.restart();
-		time=time/200;
+        double time=clock.getElapsedTime().asMicroseconds();
+        reload_time += clock.getElapsedTime().asMicroseconds();
+        clock.restart();
+        time=time/200;
 		
 		while (window.pollEvent(event))
 		{
@@ -27,9 +32,15 @@ int main()
 		}
 		if (cmonBruh.getRect().intersects(entity.getRect()))
 			cmonBruh.sprite.setColor(Color::Red); else cmonBruh.sprite.setColor(Color::White);
-		cmonBruh.cok();
+        if (Keyboard::isKeyPressed(Keyboard::Space) && (reload_time>=100000)){
+            bullets.push_back(SimpleBullet("Bullet.png", cmonBruh.x, cmonBruh.y, 14, 14));
+            reload_time = 0;
+        }
+		//cmonBruh.cok();
 		cmonBruh.control(time);
+        for(int i=0; i<bullets.size(); i++) bullets[i].move(time);
 		window.clear();
+        for(int i=0; i<bullets.size(); i++) window.draw(bullets[i].sprite);
 		window.draw(entity.sprite);
 		window.draw(cmonBruh.sprite);
 		window.display();
