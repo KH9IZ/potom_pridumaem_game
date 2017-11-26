6e3d94b0ab680d6ab6713368730420c19f605d44
KH9IZ <knyazevegor01@mail.ru>
2017-11-26 15:58:36 +0500

    Merge branch 'master' of C:\Users\knyaz\Documents\My progs\c++\potom_pridumaem_game with conflicts.

--- Egor/SimpleBullet.h
+++ Egor/SimpleBullet.h
@@ -13,11 +13,13 @@
 
 class SimpleBullet: public Entity {
 public:
-    SimpleBullet(Texture file, float X, float Y, float W, float H) : Entity(file, X, Y, W, H) { speed = 0.05;}
+    SimpleBullet(std::basic_string<char, std::char_traits<char>, std::allocator<char>> file, float X, float Y, float W, float H) : Entity(file, X, Y, W, H) { speed = 0.05;}
 
     void move(double time){
+
         y -= speed*time;
         sprite.setPosition(x, y);
+		//sprite.move(0, speed);
     }
 };
 
---
+++ Player.h
@@ -0,0 +1,33 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <SFML/Graphics.hpp>
+#include <string>
+#include "Entity.h"
+#include <iostream>
+
+using namespace sf;
+
+class Player: public Entity {
+public:
+	Player(const std::string &file,float X, float Y, float W, float H): Entity(file, X, Y, W, H) {}
+
+
+	void control(double time)
+	{
+		if (Keyboard::isKeyPressed(Keyboard::Up) && (y-speed*time > -h))
+			y -= speed*time;
+		if (Keyboard::isKeyPressed(Keyboard::Down) && (y+speed*time < 600-h))
+			y += speed*time;
+		if (Keyboard::isKeyPressed(Keyboard::Left) && (x-speed*time > -w))
+			x -= speed*time;
+		if (Keyboard::isKeyPressed(Keyboard::Right) && (x+speed*time <= 800-w))
+			x += speed*time;
+		if (Keyboard::isKeyPressed(Keyboard::LShift))
+			speed=0.025;
+		else speed=0.0375;
+		sprite.setPosition(x,y);
+	}
+};
+
+#endif
