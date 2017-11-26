4964284a39f0b4536dca317137c916dd942b8c7e
Евгений Воробьев <user-vo2@yandex.ru>
2017-11-26 21:29:06 +0500

    Merge branch 'master' of C:\potom_pridumaem_game with conflicts.

---
+++ user-vo2/SimpleBullet.h
@@ -0,0 +1,21 @@
+#ifndef SIMPLE_BULLET
+#define SIMPLE_BULLET
+
+#include <SFML/Graphics.hpp>
+#include <string>
+#include "Entity.h"
+#include <iostream>
+
+
+class SimpleBullet: public Entity {
+public:
+    SimpleBullet(const std::string &file, float X, float Y, float W, float H) : Entity(file, X, Y, W, H) {speed = 0.1;}
+
+    void move(double time){
+        y -= speed*time;
+        sprite.setPosition(x, y);
+    }
+};
+
+
+#endif
